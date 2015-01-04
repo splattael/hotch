6fbb0b302eaf4e4290afd2c48ee068ab057a299a
Peter Suschlik <peter@suschlik.de>
2015-01-04 22:57:07 +0100

    Initial quick and dirty implementation

--- lib/hotch.rb
+++ lib/hotch.rb
@@ -1,5 +1,42 @@
-require "hotch/version"
+require 'stackprof'
+require 'tmpdir'
 
-module Hotch
-  # Your code goes here...
+HOTCH_REPORTER = ENV.fetch('HOTCH_REPORTER', 'print_graphviz')
+HOTCH_VIEWER = ENV['HOTCH_VIEWER']
+
+NAME = $0.gsub(/\W/, '_')
+
+StackProf.start
+
+def system!(*args)
+  system(*args) or raise "system call failed: #{args.join(' ')}"
+end
+
+at_exit do
+  StackProf.stop
+
+  begin
+    dir = Dir.mktmpdir("hotch.#{NAME}")
+    dump = File.open(File.join(dir, "profile.dump"), "wb")
+    svg = File.join(File.join(dir, "profile.svg"))
+
+    old_verbose, $VERBOSE = $VERBOSE, nil
+    old_stdout, STDOUT = STDOUT, dump
+
+    StackProf::Report.new(StackProf.results).public_send(HOTCH_REPORTER)
+
+    dump.close
+
+    system! "dot", "-Tsvg", "-o", svg, dump.path
+
+    puts "Profile SVG: #{svg}"
+
+    if viewer = HOTCH_VIEWER
+      system! viewer, svg
+    end
+  ensure
+    STDOUT = old_stdout
+    $VERBOSE = old_verbose
+  end
+
 end
