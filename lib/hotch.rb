f8f8d29a5a7b233028016fa3f7fb48fa3473272c
Peter Suschlik <peter@suschlik.de>
2015-01-05 00:01:01 +0100

    Pin to graphviz for now

--- lib/hotch.rb
+++ lib/hotch.rb
@@ -1,7 +1,6 @@
 require 'stackprof'
 require 'tmpdir'
 
-HOTCH_REPORTER = ENV.fetch('HOTCH_REPORTER', 'print_graphviz')
 HOTCH_VIEWER = ENV['HOTCH_VIEWER']
 
 NAME = $0.gsub(/\W/, '_')
@@ -15,28 +14,19 @@ end
 at_exit do
   StackProf.stop
 
-  begin
-    dir = Dir.mktmpdir("hotch.#{NAME}")
-    dump = File.open(File.join(dir, "profile.dump"), "wb")
-    svg = File.join(File.join(dir, "profile.svg"))
+  dir = Dir.mktmpdir("hotch.#{NAME}")
+  dump = File.open(File.join(dir, "profile.dump"), "wb")
+  svg = File.join(File.join(dir, "profile.svg"))
 
-    old_verbose, $VERBOSE = $VERBOSE, nil
-    old_stdout, STDOUT = STDOUT, dump
+  StackProf::Report.new(StackProf.results).print_graphviz(nil, dump)
 
-    StackProf::Report.new(StackProf.results).public_send(HOTCH_REPORTER)
+  dump.close
 
-    dump.close
+  system! "dot", "-Tsvg", "-o", svg, dump.path
 
-    system! "dot", "-Tsvg", "-o", svg, dump.path
+  puts "Profile SVG: #{svg}"
 
-    puts "Profile SVG: #{svg}"
-
-    if viewer = HOTCH_VIEWER
-      system! viewer, svg
-    end
-  ensure
-    STDOUT = old_stdout
-    $VERBOSE = old_verbose
+  if viewer = HOTCH_VIEWER
+    system! viewer, svg
   end
-
 end
