0c540987ef2a99b6a2902f93dc0d5b24b3585d7d
Peter Suschlik <ps@neopoly.de>
2015-01-05 13:40:48 +0100

    Move reporting into hotch class

--- lib/hotch.rb
+++ lib/hotch.rb
@@ -1,7 +1,10 @@
 require 'stackprof'
 
 class Hotch
-  def initialize
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
   end
 
   def start(*args)
@@ -20,6 +23,24 @@ class Hotch
 
   def results
     StackProf.results
+  end
+
+  def report
+    dir = Dir.mktmpdir("hotch.#{name}")
+    dump = File.open(File.join(dir, "profile.dump"), "wb")
+    svg = File.join(File.join(dir, "profile.svg"))
+
+    StackProf::Report.new(results).print_graphviz(nil, dump)
+
+    dump.close
+
+    system! "dot", "-Tsvg", "-o", svg, dump.path
+
+    yield svg
+  end
+
+  def system!(*args)
+    Kernel.system(*args) or raise "system call failed: #{args.join(' ')}"
   end
 end
 
--- lib/hotch/run.rb
+++ lib/hotch/run.rb
@@ -3,31 +3,19 @@ require 'tmpdir'
 
 HOTCH_VIEWER = ENV['HOTCH_VIEWER']
 
-NAME = $0.gsub(/\W/, '_')
+program = $0.gsub(/\W/, '_')
 
-hotch = Hotch.new
+hotch = Hotch.new(program)
 hotch.start
-
-def system!(*args)
-  system(*args) or raise "system call failed: #{args.join(' ')}"
-end
 
 at_exit do
   hotch.stop
 
-  dir = Dir.mktmpdir("hotch.#{NAME}")
-  dump = File.open(File.join(dir, "profile.dump"), "wb")
-  svg = File.join(File.join(dir, "profile.svg"))
+  hotch.report do |svg|
+    puts "Profile SVG: #{svg}"
 
-  StackProf::Report.new(hotch.results).print_graphviz(nil, dump)
-
-  dump.close
-
-  system! "dot", "-Tsvg", "-o", svg, dump.path
-
-  puts "Profile SVG: #{svg}"
-
-  if viewer = HOTCH_VIEWER
-    system! viewer, svg
+    if viewer = HOTCH_VIEWER
+      system viewer, svg
+    end
   end
 end
