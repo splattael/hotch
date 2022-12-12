56b86296719042bd88fdd32187a4d57400e09432
Peter Leitzen <peter@leitzen.de>
2022-12-12 12:02:01 +0100

    Add minitest integration example

---
+++ examples/example_test.rb
@@ -0,0 +1,25 @@
+require 'minitest/autorun'
+require 'hotch/minitest'
+require 'hotch/memory/minitest'
+
+Hotch::Minitest.run
+Hotch::Memory::Minitest.run
+
+class ExampleTest < Minitest::Test
+  def foo
+    "x" * 23
+  end
+
+  def bar
+    ["x"] * 23
+  end
+
+  def test_hotch
+    10_000.times do
+      foo
+      bar
+    end
+
+    assert true
+  end
+end
